19c2fa5ad81c53d6e71842da395173bd15902231
Rui Qiu <qiu.580@osu.edu>
2022-03-22 14:57:07 -0400

    merging

--- new_gui/frontend/src/utils/KGutils.js
+++ new_gui/frontend/src/utils/KGutils.js
@@ -58,7 +58,7 @@ async function graphNodeLinkExpand(graphData, nodeId, relation, threshold) {
     relations.forEach(relation => {
         relationList.push(relation.id)
     })
-
+    
     const passingData = {nodes: nodeList, relations: relationList, expand_node: nodeId, relationship_name:relation, threshold: threshold}
     const path = "http://127.0.0.1:5000/expandNodeWithR"
     const updatedGraphData = await axios.post(path, passingData)
