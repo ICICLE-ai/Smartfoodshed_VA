f60b4eda3b0e64bab6b4efabd1a2011a90ac0cb3
Yamei Tu <244367691@qq.com>
2022-05-18 14:25:21 -0400

    ready for deployment

--- new_gui/frontend/deploy.sh
+++ new_gui/frontend/deploy.sh
@@ -10,13 +10,13 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'iciclesmartfoodshedva.com' > CNAME
+# echo 'iciclesmartfoodshedva.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yasmineTYM/PPOD_KG.git main:gh-pages
+git push -f git@github.com:yasmineTYM/PPOD_KG.git master:gh-pages
 
-cd -
+cd -gh
